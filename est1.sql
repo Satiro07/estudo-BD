61200e1fce8097dec5172b6974fb5403e8c2fad4
Satiro07 <jardelsatiro57@gmail.com>
2025-04-18 19:14:26 -0300

    Create est1.sql

---
+++ est1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projetos (
+    id SERAIL PRIMARY KEY,
+    nome VARCHAR(100),
+    descricao TEXT,
+    data_inicio DATE,
+    status VARCHAR(20)
+);
+
+INSERT INTO projetos (nome, descricao, data_inicio, status)
+VALUES
+('Projeto A', 'Um projeto ativo', '2025-04-17', 'ativo'),
+('Projeto B', 'Um projeto pendende', '2025-01-02', 'pedente'),
+('Projeto C', 'Um projeto ativo', '2024-05-25', 'ativo');
+
+SELECT * FROM projetos
+WHERE status = 'ativo';
