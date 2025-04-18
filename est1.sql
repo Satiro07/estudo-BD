42cc753161969bd2b4a8bdd90cf7e87bef1fff6d
Satiro07 <jardelsatiro57@gmail.com>
2025-04-18 19:25:33 -0300

    Update est1.sql

--- est1.sql
+++ est1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projetos (
-    id SERAIL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(100),
     descricao TEXT,
     data_inicio DATE,
@@ -9,8 +9,7 @@ CREATE TABLE projetos (
 INSERT INTO projetos (nome, descricao, data_inicio, status)
 VALUES
 ('Projeto A', 'Um projeto ativo', '2025-04-17', 'ativo'),
-('Projeto B', 'Um projeto pendende', '2025-01-02', 'pedente'),
+('Projeto B', 'Um projeto pendende', '2025-01-02', 'pendente'),
 ('Projeto C', 'Um projeto ativo', '2024-05-25', 'ativo');
 
-SELECT * FROM projetos
-WHERE status = 'ativo';
+SELECT * FROM projetos;
