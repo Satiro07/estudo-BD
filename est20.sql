020022ef6e9d2eafb22260984dc29b6455efb5ce
Satiro07 <jardelsatiro57@gmail.com>
2025-04-20 11:07:36 -0300

    Update est20.sql

--- est20.sql
+++ est20.sql
@@ -31,9 +31,27 @@ VALUES
 (3, 'pizza', 3, 30.00, '2025-04-04'),
 (3, 'salada', 1, 15.00, '2025-04-04');
 
+-- quanto cada cliente gastou ao todo
 SELECT
     c.nome AS "Nome Cliente",
     SUM(p.valor_unitario * p.quantidade) AS "Gasto Total"
 FROM pedidos p
 JOIN clientes c ON p.id_cliente = c.id
 GROUP BY c.nome;
+
+-- quantidade total de cada item vendido e faturamento
+SELECT
+    p.item AS "Nome do Item",
+    SUM(p.quantidade) AS "Quantidade vendida",
+    SUM(p.quantidade * valor_unitario) AS "Valor total gerado"
+FROM pedidos p
+GROUP BY p.item
+ORDER BY SUM(p.quantidade) DESC;
+
+-- quantidade de pedidos por dia
+SELECT 
+    COUNT(p.quantidade) AS "Quantidade de pedidos",
+    p.data_pedido AS "Data"
+FROM pedidos p
+GROUP BY p.data_pedido
+ORDER BY p.data_pedido;
