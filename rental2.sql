bf5fec7d9e10acbc2d85786a603150634d09818d
Satiro07 <jardelsatiro57@gmail.com>
2025-04-21 09:03:21 -0300

    Update rental2.sql

--- rental2.sql
+++ rental2.sql
@@ -1,4 +1,8 @@
 SELECT
-    CONCAT(c.first_name, ' ', c.last_name)
-FROM customer
+    CONCAT(c.first_name, ' ', c.last_name) AS "Nome",
+    SUM(p.amount) AS "Quantia gasta"
+FROM customer c
+INNER JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY CONCAT(c.first_name, ' ', c.last_name)
+ORDER BY SUM(p.amount) DESC
 LIMIT 5;
