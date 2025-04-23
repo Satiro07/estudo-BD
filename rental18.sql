d38d6a1532fdf58950cc3732db6df31803bc466e
Satiro07 <jardelsatiro57@gmail.com>
2025-04-22 21:30:53 -0300

    Update rental18.sql

--- rental18.sql
+++ rental18.sql
@@ -1,10 +1,10 @@
 SELECT 
     CONCAT(c.first_name, ' ', c.last_name) AS "Nome do cliente",
     MAX(p.amount) AS "Maior pagamento",
-    s.store_id AS "ID da loja"
+    a.address AS "Endereço da loja"
 FROM customer c 
 INNER JOIN payment p ON c.customer_id = p.customer_id
 INNER JOIN staff st ON p.staff_id = st.staff_id
-INNER JOIN store s ON st.store_id = s.store_id
+INNER JOIN address a ON st.address_id = aa.address_id
 GROUP BY c.first_name, c.last_name, s.store_id
 ORDER BY MAX(p.amount) DESC;
