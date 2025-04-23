7102b97083c97c18ac20edf2bdb7796844d75d21
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 16:37:02 -0300

    Update rental22.sql

--- rental22.sql
+++ rental22.sql
@@ -1,11 +1,11 @@
 SELECT 
     CONCAT(a.first_name, ' ', a.last_name) AS "Nome ator",
-    COUNT(c.name) AS "Quantidade",
-    co.country AS "País"
+    COUNT(c.name) AS "Quantidade"
 FROM film_actor fa 
 INNER JOIN actor a ON fa.actor_id = a.actor_id
 INNER JOIN film f ON fa.film_id = f.film_id
-INNER JOIN film_category fc ON c.category_id = fc.category_id
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
 GROUP BY CONCAT(a.first_name, ' ', a.last_name)
-ORDER BY COUNT(c.name) AS "Quantidade" DESC
+ORDER BY COUNT(c.name) DESC
 LIMIT 5;
