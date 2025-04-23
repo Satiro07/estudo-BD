fd12ed7ddc18420b8efad726953dcc330b9eaf12
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 16:01:13 -0300

    Update rental21.sql

--- rental21.sql
+++ rental21.sql
@@ -1,6 +1,7 @@
 SELECT 
-    c.name AS "Nome daa categoria",
-    COUNT(f.film_id) AS "Quantidade ded filmes"
+    c.name AS "Nome da categoria",
+    COUNT(f.film_id) AS "Quantidade de filmes"
 FROM film_category fc 
 INNER JOIN category c ON fc.category_id = c.category_id
 INNER JOIN film f ON fc.film_id = f.film_id
+GROUP BY c.name
