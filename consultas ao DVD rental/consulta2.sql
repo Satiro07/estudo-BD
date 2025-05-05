885892b3d6f8cd83036e3a9d8ddd5ddd8826ea9d
Satiro07 <jardelsatiro57@gmail.com>
2025-05-05 20:59:03 -0300

    Update consulta2.sql

--- consultas ao DVD rental/consulta2.sql
+++ consultas ao DVD rental/consulta2.sql
@@ -11,3 +11,12 @@ INNER JOIN payment p ON r.rental_id = p.rental_id
 GROUP BY f.title
 ORDER BY COUNT(p.amount) DESC
 LIMIT 5;
+
+-- desafio extra
+
+-- 5 filmes com mais pagamentos, mas apenas entre os clientes da cidade de "Rio de Janeiro"
+
+
+
+INNER JOIN customer c ON p.customer_id = c.customer_id
+INNER JOIN
