3dc751d84e608723433bdbc2d2f111039e452108
Satiro07 <jardelsatiro57@gmail.com>
2025-05-05 22:33:57 -0300

    Update cunsulta6.sql

---
+++ consultas ao DVD rental/cunsulta6.sql
@@ -0,0 +1,9 @@
+-- Calcule a receita total por loja.
+
+SELECT 
+    SUM(p.amount) AS "Total receita",
+    s.store_id AS "ID da loja"
+FROM staff st 
+INNER JOIN payment p ON st.staff_id = p.staff_id
+INNER JOIN store s ON st.store_id = s.store_id
+GROUP BY s.store_id
