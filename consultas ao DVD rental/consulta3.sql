44cd04430439a0c805ed5a629955cc803802c9cd
Satiro07 <jardelsatiro57@gmail.com>
2025-05-05 22:14:38 -0300

    Update consulta3.sql

--- consultas ao DVD rental/consulta3.sql
+++ consultas ao DVD rental/consulta3.sql
@@ -5,5 +5,4 @@ SELECT
     COUNT(fc.film_id) AS "Quantidade"
 FROM film_category fc 
 INNER JOIN category c ON fc.category_id = c.category_id
-INNER JOIN film f ON fc.film_id = f.film_id
 GROUP BY c.name
